0e4667cc3aef76c42a2715d3ec7bd6b4f4b91e44
Eszter Bokkon <bokkone@gmail.com>
2020-01-21 11:11:20 +0100

    Create routes in App.js.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 import "./App.css";
 
@@ -11,7 +12,48 @@ function App() {
         <header className="App-header">
           <Navbar />
         </header>
-        <Route></Route>
+        <Route
+          exact
+          path="/"
+          render={props => (
+            <React.Fragment>
+              <Home />
+            </React.Fragment>
+          )}
+        ></Route>
+        <Route
+          exact
+          path="/courses"
+          render={props => <React.Fragment></React.Fragment>}
+        ></Route>
+        <Route
+          exact
+          path="/cocktails"
+          render={props => <React.Fragment></React.Fragment>}
+        ></Route>
+        <Route
+          exact
+          path="/cocktails:id"
+          render={props => <React.Fragment></React.Fragment>}
+        ></Route>
+        <Route
+          exact
+          path="/search"
+          render={props => (
+            <React.Fragment>
+              <Home />
+            </React.Fragment>
+          )}
+        ></Route>
+        <Route
+          exact
+          path="/shoppingcart"
+          render={props => (
+            <React.Fragment>
+              <Home />
+            </React.Fragment>
+          )}
+        ></Route>
       </div>
     </BrowserRouter>
   );
