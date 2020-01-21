f06150809361aab69825558c1984d22d266d1842
Agnes Kadlot <agnes.kadlot@gmail.com>
2020-01-21 20:31:12 +0100

    Fix Http Hook

--- src/hooks/Http.js
+++ src/hooks/Http.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export const useHttp = (url, dependencies) => {
-  const [fetchedData, setFetchedData] = useState([]);
+export function useHttp(url) {
+  const [fetchedData, setFetchedData] = useState(null);
+
   useEffect(() => {
-    axios.get(url).then(response => setFetchedData(response.data));
-  }, [dependencies, url]);
-  return [fetchedData, setFetchedData];
-};
+    async function fetchData() {
+      const response = await axios(url);
+      setFetchedData(response);
+    }
+    fetchData();
+    //axios.get(url).then(response => setFetchedData(response.data));
+  }, []);
+
+  console.log(fetchedData);
+  return [fetchedData];
+}
