3edef36b81421f428362312f56ce8e1f873c82a7
Agnes Kadlot <agnes.kadlot@gmail.com>
2020-01-22 11:38:42 +0100

    Navbar Styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,121 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0
+    },
+    li: {
+      listStyle: "none"
+    }
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`
+  },
+  toolbar: {
+    flexWrap: "wrap"
+  },
+  toolbarTitle: {
+    flexGrow: 1
+  },
+  link: {
+    margin: theme.spacing(1, 1.5)
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6)
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === "dark"
+        ? theme.palette.grey[700]
+        : theme.palette.grey[200]
+  },
+  cardPricing: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
+    marginBottom: theme.spacing(2)
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6)
+    }
+  }
+}));
 
 export default function Navbar() {
+  const classes = useStyles();
   return (
-    <div style={navbarStyle}>
-      <Link style={linkStyle} to="/">
-        Home
-      </Link>
-      <Link style={linkStyle} to="/courses">
-        Courses
-      </Link>
-      <Link style={linkStyle} to="/cocktails">
-        Browse Cocktails
-      </Link>
-      <Link style={linkStyle} to="/search">
-        Search
-      </Link>
-      <div>
-        <Link style={linkStyle} to="/shoppingcart">
-          Shopping cart
+    <AppBar
+      position="static"
+      color="default"
+      elevation={0}
+      className={classes.appBar}
+    >
+      <Toolbar className={classes.toolbar}>
+        <Typography
+          variant="h6"
+          color="inherit"
+          noWrap
+          className={classes.toolbarTitle}
+        >
+          Company Intro
+        </Typography>
+        <nav>
+          <Link
+            href="/"
+            variant="button"
+            color="textPrimary"
+            className={classes.link}
+          >
+            Home
+          </Link>
+          <Link
+            href="/courses"
+            variant="button"
+            color="textPrimary"
+            className={classes.link}
+          >
+            Courses
+          </Link>
+          <Link
+            href="/cocktails"
+            variant="button"
+            color="textPrimary"
+            className={classes.link}
+          >
+            Browse Cocktails
+          </Link>
+          <Link
+            href="/search"
+            variant="button"
+            color="textPrimary"
+            className={classes.link}
+          >
+            Browse Cocktails
+          </Link>
+        </nav>
+        <Link
+          href="/shoppingcart"
+          color="primary"
+          variant="outlined"
+          className={classes.link}
+        >
+          ShoppingCart
         </Link>
-      </div>
-    </div>
+      </Toolbar>
+    </AppBar>
   );
 }
-
-const navbarStyle = {
-  background: "#333",
-  color: "#fff",
-  textAlign: "center",
-  padding: "10px"
-};
-
-const linkStyle = {
-  color: "#fff",
-  textDecoration: "none",
-  margin: "0 10px"
-};
