17129ee0fd45f0cdbf444b770685290e2bdca795
Eszter Bokkon <bokkone@gmail.com>
2020-01-22 16:46:52 +0100

    Create Autocomplete Dropdown for searching cocktails in the Search file

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,26 +1,42 @@
 import React, { useState } from "react";
 import { useHttp } from "../hooks/Http";
 import CircularProgress from "./CircularProgress";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import CocktailImage from "./CocktailImage";
+import CocktailItem from "./CocktailItem";
 
 export default function Search() {
   const [cocktails, setCocktails] = useHttp(
     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic",
     []
   );
-  const [optionsState, setOptionsState] = useState("");
+
+  const handleOnchange = (e, v) => {
+    console.log(v);
+  };
 
   return cocktails === null ? (
     <div>
       <CircularProgress />
     </div>
   ) : (
-    <div>
-      <select value={optionsState}>
-        {cocktails.data.drinks.map(cocktail => (
-          <option>{cocktail.strDrink}</option>
-        ))}
-      </select>
-      <div>{optionsState}</div>
+    <div style={{ margin: "15px", padding: "auto" }}>
+      <Autocomplete
+        id="cocktail-select"
+        options={cocktails.data.drinks}
+        getOptionLabel={option => option.strDrink}
+        style={{ width: 300 }}
+        onChange={(event, value) => handleOnchange(event, value)}
+        renderInput={params => (
+          <TextField
+            {...params}
+            label="Search cocktails"
+            variant="outlined"
+            fullWidth
+          />
+        )}
+      />
     </div>
   );
 }
