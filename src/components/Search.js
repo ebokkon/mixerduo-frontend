c6bad49cf33e2b725d1bfedb0c1560c04e295f08
Eszter Bokkon <bokkone@gmail.com>
2020-01-23 13:59:57 +0100

    Modify search input, not working yet

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,37 +1,52 @@
-import React, { useState } from "react";
-import { useHttp } from "../hooks/Http";
+import React, { useState, Link } from "react";
 import CircularProgress from "./CircularProgress";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import CocktailImage from "./CocktailImage";
 import CocktailItem from "./CocktailItem";
+import CardActions from "@material-ui/core/CardActions";
+import { Card } from "@material-ui/core";
+import { CardContent } from "@material-ui/core";
+import { CardActionArea } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
 
 export default function Search(props) {
-  const [cocktails, setCocktails] = useHttp(
-    "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic",
-    []
-  );
+  // const [cocktails, setCocktails] = useHttp(
+  //   "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic",
+  //   []
+  // );
 
   const [selectedCocktail, setSelectedCocktail] = useState("");
-  const [selectedCocktailName, setSelectedCocktailName] = useState("");
 
   const handleOnchange = (event, value) => {
     value === null ? setSelectedCocktail(null) : setSelectedCocktail(value);
-    value === null
-      ? setSelectedCocktailName("")
-      : setSelectedCocktailName(value.strDrink);
   };
 
-  return cocktails === null ? (
+  const useStyles = makeStyles({
+    card: {
+      maxWidth: 345,
+      margin: "30px",
+      display: "inline-block"
+    },
+    link: {
+      textDecoration: "none"
+    }
+  });
+  const classes = useStyles();
+
+  return props.cocktails === null ? (
     <div>
       <CircularProgress />
     </div>
   ) : (
     <div>
+      <div> {console.log(props.cocktails)}</div>
       <div style={{ margin: "15px", padding: "auto" }}>
         <Autocomplete
           id="cocktail-select"
-          options={cocktails.data.drinks}
+          options={props.cocktails}
           getOptionLabel={option => option.strDrink}
           style={{ width: 400 }}
           onChange={(event, value) => handleOnchange(event, value)}
@@ -47,20 +62,51 @@ export default function Search(props) {
       </div>
       <div>
         {selectedCocktail === null ? (
-          cocktails.data.drinks.map(cocktail => (
-            <React.Fragment key={cocktail.idDrink}>
-              <CocktailImage image={cocktail.strDrinkThumb} />
-              <CocktailItem item={cocktail} handleClick={props.handleClick} />
-            </React.Fragment>
+          props.cocktails.data.drinks.map(cocktail => (
+            <Card className={classes.card} key={cocktail.idDrink}>
+              <CardActionArea key={cocktail.idDrink}>
+                <CocktailImage image={cocktail.strDrinkThumb} />
+                <CardContent>
+                  <CocktailItem item={cocktail} />
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <FavoriteIcon size="small" color="primary" />
+                <ShareIcon size="small" color="primary"></ShareIcon>
+                <Link
+                  className={classes.link}
+                  to={`/cocktails/${cocktail.idDrink}`}
+                  id={cocktail.idDrink}
+                  size="small"
+                  color="primary"
+                >
+                  Learn More
+                </Link>
+              </CardActions>
+            </Card>
           ))
         ) : (
-          <React.Fragment>
-            <CocktailImage image={selectedCocktail.strDrinkThumb} />
-            <CocktailItem
-              item={selectedCocktail}
-              handleClick={props.handleClick}
-            />
-          </React.Fragment>
+          <Card className={classes.card} key={selectedCocktail.idDrink}>
+            <CardActionArea key={selectedCocktail.idDrink}>
+              <CocktailImage image={selectedCocktail.strDrinkThumb} />
+              <CardContent>
+                <CocktailItem item={selectedCocktail} />
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <FavoriteIcon size="small" color="primary" />
+              <ShareIcon size="small" color="primary"></ShareIcon>
+              {/* <Link
+                className={classes.link}
+                to={`/cocktails/${selectedCocktail.idDrink}`}
+                id={selectedCocktail.idDrink}
+                size="small"
+                color="primary"
+              >
+                Learn More
+              </Link> */}
+            </CardActions>
+          </Card>
         )}
       </div>
     </div>
