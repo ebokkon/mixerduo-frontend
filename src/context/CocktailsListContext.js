accd7737f64d69820a8b733a4fb456e18b666ded
Eszter Bokkon <bokkone@gmail.com>
2020-02-05 15:57:13 +0100

    Change useEffect function in CocktailsListContext component, so that frontend gets the data from the API call of the backend.

--- src/context/CocktailsListContext.js
+++ src/context/CocktailsListContext.js
@@ -7,25 +7,10 @@ export const CocktailsListProvider = props => {
   const [cocktails, setCocktails] = useState([]);
 
   useEffect(() => {
-    let temp = [];
-    let a = 97;
-    let letters = [];
-    for (let i = 0; i < 26; i++) {
-      letters.push(String.fromCharCode(a + i));
-    }
-    Promise.all(
-      letters.map(letter =>
-        axios
-          .get(
-            `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`
-          )
-          .then(result => (temp = temp.concat(result.data.drinks)))
-      )
-    )
-      .then(result => setCocktails(temp.filter(z => z !== null)))
-      .then(console.log(cocktails))
-      .catch(e => console.log(e));
+    axios.get("http://localhost:8080").then(response => setCocktails(response.data))
   }, []);
+
+  console.log(cocktails);
 
   return (
     <CocktailsListContext.Provider value={{ cocktails, setCocktails }}>
