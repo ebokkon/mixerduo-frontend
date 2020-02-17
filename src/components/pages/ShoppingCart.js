d3fb1ad4fac26d1c2d83c7fcd45d0ed5eb1c3194
Agnes Kadlot <agnes.kadlot@gmail.com>
2020-02-17 19:39:35 +0100

    Add buttons to navigate to checkout page

--- src/components/pages/ShoppingCart.js
+++ src/components/pages/ShoppingCart.js
@@ -2,14 +2,23 @@ import React, { useContext } from "react";
 import { ShoppingCartContext } from "../../context/ShoppingCartContext";
 import SimpleTable from "./shoppingcartunits/SimpleTable";
 import ShoppingCartMessage from "./shoppingcartunits/ShoppingCartMessage";
+import Button from "@material-ui/core/Button";
+import {Link as RouterLink} from "react-router-dom";
+import Link from "@material-ui/core/Link";
 
 export default function ShoppingCart() {
   const { cart, handleCart } = useContext(ShoppingCartContext);
 
   return cart.length === 0 ? (
-    <ShoppingCartMessage/>
+      <React.Fragment>
+        <ShoppingCartMessage/>
+        <Button variant="outlined" color="primary" component={RouterLink} to="/courses">Checkout our Courses!</Button>
+      </React.Fragment>
   ) : (
-    <SimpleTable cart={cart} />
+     <React.Fragment>
+       <SimpleTable cart={cart} />
+        <Button variant="outlined" color="primary" component={RouterLink} to="/shoppingcart/checkout">Checkout</Button>
+     </React.Fragment>
   );
 }
 
